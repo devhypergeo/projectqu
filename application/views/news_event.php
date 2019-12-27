7102e19b222412e8cd6ac2e99ec40116ba1a9739
devhypergeo <59184125+devhypergeo@users.noreply.github.com>
2019-12-27 14:51:50 +0700

    update news and event

---
+++ application/views/news_event.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>News & Event</title>
+	</head>
+	<body>
+		<h1>News & Event</h1>
+		<ul>
+			<li><a href="home">Home</a></li>
+			<li><a href="about">About</a></li>
+			<li><a href="newsevent">News & Event</a></li>
+		</ul>
+		<p>Ini halaman news & event</p>
+	</body>
+</html>
