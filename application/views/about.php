9e7db265915100a885a80126f21c74687ede5c02
devhypergeo <59184125+devhypergeo@users.noreply.github.com>
2019-12-27 14:45:39 +0700

    Update about.php

--- application/views/about.php
+++ application/views/about.php
@@ -8,7 +8,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</head>
 	<body>
 		<h1>About</h1>
-		<a href="home">Home</a>
-		Ini Adalah Halaman Branch
+		<ul>
+			<li><a href="home">Home</a></li>
+			<li><a href="about">About</a></li>
+			<li><a href="newsevent">News & Event</a></li>
+		</ul>
+		Ini adalah halaman about
 	</body>
 </html>
