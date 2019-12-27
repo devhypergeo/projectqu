d51a07a5bc846dbcbf9fe954ef1bd18e05d81d7a
devhypergeo <59184125+devhypergeo@users.noreply.github.com>
2019-12-27 14:44:58 +0700

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -8,7 +8,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</head>
 	<body>
 		<h1>Home</h1>
-		<a href="about">About</a>
-		Ini halaman home
+		<ul>
+			<li><a href="home">Home</a></li>
+			<li><a href="about">About</a></li>
+			<li><a href="newsevent">News & Event</a></li>
+		</ul>
+		<p>Ini halaman home</p>
 	</body>
 </html>
